eac72a9160dd4f1e870bdedeea7de1fc9ddfaa4e
KBK <67410146+Kwon-Bum-Kyu@users.noreply.github.com>
2026-04-20 00:23:55 +0900

    chore: 워크스페이스 lint 설정 정리 및 jest-dom 의존성 추가

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,8 +1,6 @@
 import { config as reactConfig } from "@repo/eslint-config/react";
-import storybook from "eslint-plugin-storybook";
 
 /** @type {import("eslint").Linter.Config} */
 export default [
   ...reactConfig,
-  ...storybook.configs["flat/recommended"],
 ];
